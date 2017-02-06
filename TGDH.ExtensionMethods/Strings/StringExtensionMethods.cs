d7f798b61ed55750debd836d68688ce4c534393f
Kev Simpson <hello@kev-simpson.me>
2017-02-06 17:11:00 +0000

    Add extensions methods and core with paging

---
+++ TGDH.ExtensionMethods/Strings/StringExtensionMethods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace TGDH.ExtensionMethods.Strings
+{
+    public static class StringExtensionMethods
+    {
+        private static readonly string LettersAndDigitsPattern = @"[^0-9a-zA-Z]+";
+
+        /// <summary>
+        /// Checks if the given email address is a valid MailAddress
+        /// </summary>
+        /// <param name="email">Email address</param>
+        /// <returns>True or false</returns>
+        public static bool IsValidEmail(this string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email.Trim());
+                return addr.Address == email;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Converts the given string to 0-9 and a-z format
+        /// </summary>
+        /// <param name="source">String to convert</param>
+        /// <returns>Converted lowercase string</returns>
+        public static string ConvertToId(this string source)
+        {
+            return !string.IsNullOrWhiteSpace(source) ? Regex.Replace(source, LettersAndDigitsPattern, string.Empty).ToLower() : "";
+        }
+
+        /// <summary>
+        /// Creates a new Guid
+        /// </summary>
+        /// <returns>A unique Guid</returns>
+        public static string CreateGuid()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+
+        //public static bool IsValidUrl(this string source)
+        //{
+        //    Uri uriResult;
+        //    return Uri.TryCreate(source, UriKind.Absolute, out uriResult) && uriResult.Scheme == Uri.UriSchemeHttp;
+        //}
+    }
+}
