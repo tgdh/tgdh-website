8c0e772770cfb4a533d095c00f7aea8517a015d7
Kev <kev@Kevs-MacBook-Pro.local>
2017-03-09 16:45:20 +0000

    Add initial team to about

---
+++ TGDH.Web/App_Code/ProfileCard.cs
@@ -0,0 +1,15 @@
+using System.Web;
+
+public class ProfileCardViewModel
+{
+    public int ImageId { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string JobTitle { get; set; }
+
+    public string ModifierClass { get; set; }
+
+}
