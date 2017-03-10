5fe0df99618d9080d724982255bc59e010b3d3d2
Kev <kev@Kevs-MacBook-Pro.local>
2017-03-10 11:37:40 +0000

    Refactor social channels to be component

--- TGDH.Web/App_Code/ProfileCardViewModel.cs
+++ TGDH.Web/App_Code/ProfileCardViewModel.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Web;
+using Umbraco.Core.Models;
 
 public class ProfileCardViewModel
 {
@@ -11,5 +13,9 @@ public class ProfileCardViewModel
     public string JobTitle { get; set; }
 
     public string ModifierClass { get; set; }
+
+    public IEnumerable<IPublishedContent> SocialChannels { get; set; }
+
+    public int SocialChannelIconSize { get; set; }
 
 }
