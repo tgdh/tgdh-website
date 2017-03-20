acae0aee5d5215f706cd00aaf1f4ffd2ce52ea84
Kev <kev@Kevs-MacBook-Pro.local>
2017-03-20 15:11:47 +0000

    Add author component

---
+++ TGDH.Web/App_Code/AuthorViewModel.cs
@@ -0,0 +1,11 @@
+public class AuthorViewModel
+{
+    public int ImageId { get; set; }
+
+    public string AuthorName { get; set; }
+
+    public string JobTitle { get; set; }
+
+    public string ModifierClass { get; set; }
+
+}
