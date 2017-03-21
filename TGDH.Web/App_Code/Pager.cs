5aa7e81a8b869b3216acbb9f4f47ec87bf654aa9
Kev <kev@Kevs-MacBook-Pro.local>
2017-03-21 12:58:46 +0000

    Add pager to case study and update hull truck pager

---
+++ TGDH.Web/App_Code/Pager.cs
@@ -0,0 +1,28 @@
+public class PagerViewModel
+{
+	// previous
+    public string HeadlinePrevious { get; set; }
+
+    public string SubtitlePrevious { get; set; }
+
+    public string UrlPrevious { get; set; }
+
+    public int ImageIdPrevious { get; set; }
+
+	// next
+    public string HeadlineNext { get; set; }
+
+    public string SubtitleNext { get; set; }
+
+    public string UrlNext { get; set; }
+
+    public int ImageIdNext { get; set; }
+
+	// gateway
+    public string HeadlineGateway { get; set; }
+
+    public string UrlGateway { get; set; }
+
+	public string ModifierClass { get; set; }
+
+}
---
+++ TGDH.Web/App_Code/StatViewModel.cs
@@ -0,0 +1,9 @@
+public class StatViewModel
+{
+    public string Label { get; set; }
+
+    public string Value { get; set; }
+
+    public string ModifierClass { get; set; }
+
+}
