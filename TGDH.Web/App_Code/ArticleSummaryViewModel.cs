fb0334af0199b4f6dfd8ceba4045eaae6ee3f988
Kev <hello@kev-simpson.me>
2017-06-22 13:44:37 +0100

    Merge branch 'dev' into animation

--- TGDH.Web/App_Code/ArticleSummaryViewModel.cs
+++ TGDH.Web/App_Code/ArticleSummaryViewModel.cs
@@ -1,10 +1,14 @@
-using System.Web;
+using Umbraco.Core.Models;
 
 public class ArticleSummaryViewModel
 {
     public string Headline { get; set; }
 
 	public string Subtitle { get; set; }
+
+	public IPublishedContent Thumbnail { get; set; }
+
+	public string ThumbnailLabel { get; set; }
 
 	public string Url { get; set; }
 
